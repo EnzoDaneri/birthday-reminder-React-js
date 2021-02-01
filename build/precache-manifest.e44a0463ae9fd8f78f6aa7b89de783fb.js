0a390950019a6c0ecaa5d4ef28e3553e28faccb9
EnzoDaneri <enzodaneri@hotmail.com>
2021-02-01 14:49:35 -0300

    build for deploy

---
+++ build/precache-manifest.e44a0463ae9fd8f78f6aa7b89de783fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56080fe9d48580f289d7e94271ab27c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "784a6397d9c7f81496ce",
+    "url": "/static/css/main.bc3c73a3.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784a6397d9c7f81496ce",
+    "url": "/static/js/main.a99bc024.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
